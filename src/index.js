d362ff0819680a786fa2b9f3e6fb62f6be853883
Sebastian Landwehr <sebastianlandw@gmail.com>
2020-09-10 01:55:43 +0200

    fix: add all fields to json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import jwtDecode from 'jwt-decode'
 export default async token => {
   const user = token |> jwtDecode |> await
   return {
+    ...user,
     email: user.email,
     emailVerified: user.email_verified,
     id: user.user_id,
